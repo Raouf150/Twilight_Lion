35c1429309808b8558a1b7dc7944cb34f5468e07
raouf <raoufelashry@gmail.com>
2025-09-17 14:33:39 +0300

    Install Essentials

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+sudo pacman -Syu # Updates
+sudo pacman -S hyprland wget cliphist openssh tlp brightnessctl pauvucontrol fuse # Essentials 
 # Downloading Obsidian
-#wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.9.12/Obsidian-1.9.12.AppImage
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.9.12/Obsidian-1.9.12.AppImage
+chmod +x Obsidian
+mv Obsidian.* /usr/local/bin/obsidian # Moving obsidian to PATH
 
 

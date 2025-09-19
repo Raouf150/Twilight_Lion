ce6a4f20947b836aa9fb938e9c8eafea0d3d0cc8
raouf <raoufelashry@gmail.com>
2025-09-19 09:20:35 +0300

    Fixed hypr configuration faliures

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 #sudo pacman -Syu # Updates
-#sudo pacman -S hyprland hyprpaper wget cliphist openssh tlp brightnessctl pavucontrol fuse kitty nano firefox  # Essentials 
+#sudo pacman -S hyprland hyprpaper wget cliphist openssh tlp brightnessctl pavucontrol fuse kitty starship nano firefox  fastfetch # Essentials 
 
 # Downloading Obsidian
 #wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.9.12/Obsidian-1.9.12.AppImage
 #chmod +x Obsidian
 #mv Obsidian* /usr/local/bin/obsidian # Moving obsidian to PATH
 
+cp ~/.config ~/config.bak
+rm ~/.config/hypr -r
 mv bash ~/.bashrc
-mv kitty obsidian rofi hypr .config/
+mv -f kitty rofi hypr ~/.config/
